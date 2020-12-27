09977e865166862c855ef0a497ffbec7f6a83f87
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2020-12-27 19:31:03 +0600

    readme.md edited

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Learning Django
 
+Learning Django framework for backend web development. Getting help from 
+all the sources available to me. My primary reference book is "Django 3 by Example".
+
+
+> Learn Until you die 
 
 **References**
 -----------------------
@@ -7,4 +12,4 @@
 | ---------------------|
 | Django 3 by Example(Third Edition) |
 | Random Google Search topics|
-| Random Youtube videos |
+| Random Youtube videos(Specially Thanks to : [Corey Schafer](https://www.youtube.com/channel/UCCezIgC97PvUuR4_gbFUs5g))|
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.core.mail import send_mail
-from django.shortcuts import render, get_object_or_404
-
-from .forms import EmailPostForm, CommentForm
-from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
+
+from .forms import EmailPostForm, CommentForm
+from .models import Post
 
 
 # render all post list
@@ -56,12 +57,19 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
         else:
             comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 # class base view 
